0951a210c278a844cd6ef0a5925ee39c069a8af9
jannunzi <jannunzi@gmail.com>
2018-06-20 23:17:50 -0400

    quizzes

--- server.js
+++ server.js
@@ -67,5 +67,6 @@ var userService = require('./services/user.service.server');
 userService(app);
 
 require('./services/section.service.server')(app);
+require('./services/quiz.service.server')(app);
 
 app.listen(4000);
